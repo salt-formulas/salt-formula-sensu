950c9c9610f7df13e6717bfa2e8bd1bfad0d10f8
vmikes <vlastimil.mikes@tcpcloud.eu>
2016-06-14 16:14:25 +0200

    rabbitmq_stuck_nodes check, update sudoer

---
+++ sensu/files/checks/check_rabbitmq_stuck_nodes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#checks "rabbitmqctl eval 'rabbit_diagnostics:maybe_stuck().'" command.
+
+exit_critical() {
+    echo "CRITICAL: $*"
+    exit 2
+}
+exit_ok() {
+    echo "OK: $*"
+    exit 0
+}
+
+read -ra rabbit_eval <<< $(sudo rabbitmqctl eval 'rabbit_diagnostics:maybe_stuck().' | grep -o "Found "." suspicious processes.")
+if [[ -n ${rabbit_eval[@]} ]]; then
+	if [[ ${rabbit_eval[1]} -eq 0 ]]; then
+		exit_ok ${rabbit_eval[@]}
+	else
+		exit_critical ${rabbit_eval[@]}
+	fi
+else
+	exit_critical "rabbit_diagnostic failed"
+fi
+
